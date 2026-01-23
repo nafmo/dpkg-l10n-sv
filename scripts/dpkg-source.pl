nafmo/dpkg-l10n-sv

commit a77d00af63a2c8084df991c360d84280e52428c5

scripts: Parse and validate Maintainer and Uploaders email addresses
Make sure the data that we ingest follows the expected format, and that
the data that we produce has been sanitized if necessary.

scripts/dpkg-source.pl
@@ -442,6 +442,17 @@ if ($options{opmode} =~ /^(build|print-format|(before|after)-build|commit)$/) {
         }
     }
 
+    my $maint;
+    if (defined $fields->{'Maintainer'}) {
+        $maint = field_parse_maintainer($fields);
+        $fields->{'Maintainer'} = $maint->as_string();
+    }
+
+    if (defined $fields->{'Uploaders'}) {
+        my $uploaders = field_parse_uploaders($fields);
+        $fields->{'Uploaders'} = $uploaders->as_string();
+    }
+
     $fields->{'Binary'} = join(', ', @binarypackages);
     # Avoid overly long line by splitting over multiple lines.
     if (length($fields->{'Binary'}) > 980) {

scripts/dpkg-genchanges.pl
@@ -517,6 +517,11 @@ if ($fields->{'Version'} ne $substvars->get('source:Version')) {
 $fields->{'Maintainer'} = $forcemaint if defined($forcemaint);
 $fields->{'Changed-By'} = $forcechangedby if defined($forcechangedby);
 
+if (defined $fields->{'Maintainer'}) {
+    my $maint = field_parse_maintainer($fields);
+    $fields->{'Maintainer'} = $maint->as_string();
+}
+
 for my $f (qw(Version Distribution Maintainer Changes)) {
     error(g_('missing information for critical output field %s'), $f)
         unless defined $fields->{$f};

scripts/dpkg-gencontrol.pl
@@ -335,6 +335,11 @@ for my $f (qw(Priority)) {
     $fields->{$f} //= field_get_default_value($f);
 }
 
+if (defined $fields->{'Maintainer'}) {
+    my $maint = field_parse_maintainer($fields);
+    $fields->{'Maintainer'} = $maint->as_string();
+}
+
 my $pkg_type = $pkg->{'Package-Type'} ||
                $pkg->get_custom_field('Package-Type') || 'deb';
 

commit 69b05bf23ca28f2ea99ac3c395206c434e3fa068

dpkg-source: Do not error out on empty fields
The documentation for debian/control states that empty fields are
ignored there, which makes sense for fields that contain substvars,
but not so much for other fields. But as this is a long standing
documented behavior, it should be considered a regression, given
that it is making other packages unintentionally fail to build.

Fixes: commit a77d00af63a2c8084df991c360d84280e52428c5
Closes: #1125985

scripts/dpkg-source.pl
@@ -443,12 +443,12 @@ if ($options{opmode} =~ /^(build|print-format|(before|after)-build|commit)$/) {
     }
 
     my $maint;
-    if (defined $fields->{'Maintainer'}) {
+    if (length $fields->{'Maintainer'}) {
         $maint = field_parse_maintainer($fields);
         $fields->{'Maintainer'} = $maint->as_string();
     }
 
-    if (defined $fields->{'Uploaders'}) {
+    if (length $fields->{'Uploaders'}) {
         my $uploaders = field_parse_uploaders($fields);
         $fields->{'Uploaders'} = $uploaders->as_string();
 